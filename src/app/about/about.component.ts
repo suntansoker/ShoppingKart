3bea4f9c9668dd539407f0931ea2a0719c636389
suntansoker123 <sayantanthepointer@gmail.com>
2019-09-13 00:39:51 +0530

    Styling first draft

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  template: '<h3>About: This application provides the product inventory information.</h3>'
+  templateUrl: './about.component.html'
+  //styleUrls: ['./customer-detail.component.css']
 })
 
 export class AboutComponent { 
